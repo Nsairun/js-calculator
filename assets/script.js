a7507a68479f85603f19d16806e62a937cd7841d
unknown <emmansairun@gmail.com>
2023-07-04 13:21:31 +0100

    formatted my script.js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,28 @@
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const valueEl = document.querySelector('.value');
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const valueEl = document.querySelector('.value')
 
-const acEl = document.querySelector('.ac');
-const pmEl = document.querySelector('.pm');
-const percentEl = document.querySelector('.percent');
+const acEl = document.querySelector('.ac')
+const pmEl = document.querySelector('.pm')
+const percentEl = document.querySelector('.percent')
 
-const additionEl = document.querySelector('.addition');
-const subtractionEl = document.querySelector('.subtraction');
-const multiplicationEl = document.querySelector('.multiplication');
-const divisionEl = document.querySelector('.division');
-const equalEl = document.querySelector('.equal');
+const additionEl = document.querySelector('.addition')
+const subtractionEl = document.querySelector('.subtraction')
+const multiplicationEl = document.querySelector('.multiplication')
+const divisionEl = document.querySelector('.division')
+const equalEl = document.querySelector('.equal')
 
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number-0');
-const number1El = document.querySelector('.number-1');
-const number2El = document.querySelector('.number-2');
-const number3El = document.querySelector('.number-3');
-const number4El = document.querySelector('.number-4');
-const number5El = document.querySelector('.number-5');
-const number6El = document.querySelector('.number-6');
-const number7El = document.querySelector('.number-7');
-const number8El = document.querySelector('.number-8');
-const number9El = document.querySelector('.number-9');
+const decimalEl = document.querySelector('.decimal')
+const number0El = document.querySelector('.number-0')
+const number1El = document.querySelector('.number-1')
+const number2El = document.querySelector('.number-2')
+const number3El = document.querySelector('.number-3')
+const number4El = document.querySelector('.number-4')
+const number5El = document.querySelector('.number-5')
+const number6El = document.querySelector('.number-6')
+const number7El = document.querySelector('.number-7')
+const number8El = document.querySelector('.number-8')
+const number9El = document.querySelector('.number-9')
 const numberElArray = [
   number0El,
   number1El,
@@ -34,10 +34,10 @@ const numberElArray = [
   number7El,
   number8El,
   number9El
-];
+]
 
-let valueStrInMemory = null;
-let operatorInMemory = null;
+let valueStrInMemory = null
+let operatorInMemory = null
 
 const getValueAsStr = () => valueEl.textContent.split(',').join('')
 
